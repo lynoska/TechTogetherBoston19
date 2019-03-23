7d348df116b35d59d4d99b5eeba5ea9b84336bab
lynoska <lynoskagarcia@mail.adelphi.edu>
2019-03-23 14:53:47 -0400

    setup firebase to setup

--- app.py
+++ app.py
@@ -2,14 +2,30 @@ from flask import Flask, render_template, url_for, request, session, redirect, j
 from flask_bootstrap import Bootstrap
 from datetime import datetime
 import json
+import pyrebase
+import config
 
 # Init processes
 app = Flask(__name__)
 Bootstrap(app)
 
+firebase = pyrebase.initialize_app({
+	"apiKey" : config.apiKey,
+	"authDomain" : config.authDomain,
+	"databaseURL" : config.databaseURL,
+	"storageBucket" : config.storageBucket
+	})
+
+db = firebase.database()
+
+# all_user = db.child("post")
+
+
 # Route to index page
 @app.route('/')
 def index():
+	posts = db.child("post").get()
+	print(posts.val())
 	return render_template('index.html')
 
 # Route to Resources page page
@@ -18,12 +34,14 @@ def resources():
 	return render_template('resources.html')
 
 # Route to main Forum page
-@app.route('/forum')
+@app.route('/forum', methods=['GET'])
 def forum():
-	return render_template('forum.html')
+	posts = db.child("post").get().val()
+
+	return render_template('forum.html', posts=posts)
 
 # Route to main Forum page
-@app.route('/create-post')
+@app.route('/create-post', methods=['POST'])
 def post():
 	return render_template('create-post.html')
 
