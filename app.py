ae52d7e9e06fffaff7e2d302e109054597db2fdc
lynoska <lynoskagarcia@mail.adelphi.edu>
2019-03-23 04:14:23 -0400

    initial set-up for flask

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for, request, session, redirect, jsonify, flash
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+import json
+
+# Init processes
+app = Flask(__name__)
+Bootstrap(app)
+
+# Route to index page
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+# Route to Resources page page
+@app.route('/resources')
+def resources():
+	return render_template('resources.html')
+
+# Route to main Forum page
+@app.route('/forum')
+def forum():
+	return render_template('forum.html')
+
+
+if __name__ == '__main__':
+	app.run('localhost', 8080, debug=True)
